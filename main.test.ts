1232588737c0be2ff945751e3688875ac9faff72
Stefan Boos <kontakt@boos.systems>
2023-11-24 05:36:57 +0100

    feat: rename plugin, add license

--- main.test.ts
+++ main.test.ts
@@ -1,39 +1,22 @@
-import MyPlugin from "./main";
+import VendingMachinePlugin from "./main";
 import { App, PluginManifest } from "obsidian";
 
 jest.mock("obsidian");
 (window.setInterval as unknown) = jest.fn();
 
-describe("MyPlugin", () => {
-	let plugin: MyPlugin;
+describe("VendingMachinePlugin", () => {
+	let plugin: VendingMachinePlugin;
 	beforeEach(async () => {
-		plugin = new MyPlugin(new App(), {} as PluginManifest);
+		plugin = new VendingMachinePlugin(new App(), {} as PluginManifest);
 	});
 
-	it("Should register two open modal commands", async () => {
+	it("should register open command", async () => {
 		const addCommandSpy = jest.spyOn(plugin, "addCommand");
 		await plugin.onload();
 		expect(addCommandSpy).toHaveBeenCalledWith({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
+			id: "open-vending-machine",
+			name: "Open Vending Machine",
 			callback: expect.any(Function),
 		});
-		expect(addCommandSpy).toHaveBeenCalledWith({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: expect.any(Function),
-		});
-	});
-
-	it("Should register a settings tab", async () => {
-		const addSettingSpy = jest.spyOn(plugin, "addSettingTab");
-		await plugin.onload();
-		expect(addSettingSpy).toHaveBeenCalledWith(expect.any(Object));
-	});
-
-	it("Should load data and save as settings", async () => {
-		const loadDataSpy = jest.spyOn(plugin, "loadData");
-		await plugin.onload();
-		expect(loadDataSpy).toHaveBeenCalled();
 	});
 });
